d14bf87a9cbd0b094f6025aa94a1c3bed7ae03e0
Meetheora <xenotimedev@gmail.com>
2026-01-13 16:10:17 +0100

    Exercise 16

---
+++ exo_16.rb
@@ -0,0 +1,10 @@
+puts "Salut, bienvenue dans ma super pyramide !"
+etage = nil
+while etage.nil? || etage < 1 || etage > 25
+  puts "Combien d'étages veux-tu?"
+  etage = gets.chomp.to_i
+end
+
+etage.times do |i|
+  puts "#" *  (etage - i)
+end
