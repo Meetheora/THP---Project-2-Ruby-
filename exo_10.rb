754c3cd110446fdc0d2a822eea71ab3dd881e0f5
Meetheora <xenotimedev@gmail.com>
2026-01-13 12:09:18 +0100

    Exercise 10

--- exo_10.rb
+++ exo_10.rb
@@ -1,5 +1,5 @@
 puts "Quelle est votre année de naissance?"
 x = gets.chomp.to_i
 (x..2025).each do |i|
-  puts "En#{i.to_s} vous aviez #{i - x} ans"
+  puts "En #{i.to_s} vous aviez #{i - x} ans"
 end
